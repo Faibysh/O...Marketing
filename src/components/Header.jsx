ac711718cff5887866d5e5b3c685290e22bb327f
Faibysh <faybysh.b@gmail.com>
2023-06-21 12:12:24 +0200

    Fixed: burger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,72 +1,3 @@
-// import styles from "./Header.module.css";
-// import Logo from "../images/logo1.svg";
-
-// const Header = () => {
-//   return (
-//     <header className={styles.header}>
-//       <div className={styles["header__top"]}>
-//         <div className={styles.container}>
-//           <div className={styles["header__top-inner"]}>
-//             <a className={styles.logo} href="#">
-//               <img src={Logo} alt="Logo" />
-//             </a>
-//             <nav className={styles.menu}>
-//               <button className={styles["menu__btn"]}>
-//                 <svg
-//                   xmlns="http://www.w3.org/2000/svg"
-//                   width="30"
-//                   height="18"
-//                   viewBox="0 0 30 18"
-//                   fill="none"
-//                 >
-//                   <path d="M0 0H30V2H0V0Z" fill="black"></path>
-//                   <path d="M0 8H30V10H0V8Z" fill="black"></path>
-//                   <rect y="16" width="30" height="2" fill="black"></rect>
-//                 </svg>
-//               </button>
-//               <ul className={styles["menu__list"]}>
-//                 <li className={styles["menu__list-item"]}>
-//                   <a className={styles["menu__list-link"]} href="#">
-//                     HOME
-//                   </a>
-//                 </li>
-//                 <li className={styles["menu__list-item"]}>
-//                   <a className={styles["menu__list-link"]} href="#about">
-//                     ABOUT US
-//                   </a>
-//                 </li>
-//                 <li className={styles["menu__list-item"]}>
-//                   <a className={styles["menu__list-link"]} href="#portfolio">
-//                     PORTFOLIO
-//                   </a>
-//                 </li>
-//                 <li className={styles["menu__list-item"]}>
-//                   <a className={styles["menu__list-link"]} href="#blog">
-//                     BLOG
-//                   </a>
-//                 </li>
-//                 <li className={styles["menu__list-item"]}>
-//                   <a className={styles["menu__list-link"]} href="#team">
-//                     OUR TEAM
-//                   </a>
-//                 </li>
-
-//                 <li className={styles["menu__list-item"]}>
-//                   <a className={styles["menu__list-link"]} href="#contact">
-//                     CONTACT
-//                   </a>
-//                 </li>
-//               </ul>
-//             </nav>
-//           </div>
-//         </div>
-//       </div>
-//     </header>
-//   );
-// };
-
-// export default Header;
-
 import { useState } from "react";
 import styles from "./Header.module.css";
 import Logo from "../images/logo1.svg";
@@ -76,6 +7,10 @@ const Header = () => {
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleMenuLinkClick = () => {
+    setIsMenuOpen(false);
   };
 
   return (
@@ -101,32 +36,56 @@ const Header = () => {
                 }`}
               >
                 <li className={styles["menu__list-item"]}>
-                  <a className={styles["menu__list-link"]} href="#">
+                  <a
+                    className={styles["menu__list-link"]}
+                    href="#"
+                    onClick={handleMenuLinkClick}
+                  >
                     HOME
                   </a>
                 </li>
                 <li className={styles["menu__list-item"]}>
-                  <a className={styles["menu__list-link"]} href="#about">
+                  <a
+                    className={styles["menu__list-link"]}
+                    href="#about"
+                    onClick={handleMenuLinkClick}
+                  >
                     ABOUT US
                   </a>
                 </li>
                 <li className={styles["menu__list-item"]}>
-                  <a className={styles["menu__list-link"]} href="#portfolio">
+                  <a
+                    className={styles["menu__list-link"]}
+                    href="#portfolio"
+                    onClick={handleMenuLinkClick}
+                  >
                     PORTFOLIO
                   </a>
                 </li>
                 <li className={styles["menu__list-item"]}>
-                  <a className={styles["menu__list-link"]} href="#blog">
+                  <a
+                    className={styles["menu__list-link"]}
+                    href="#blog"
+                    onClick={handleMenuLinkClick}
+                  >
                     BLOG
                   </a>
                 </li>
                 <li className={styles["menu__list-item"]}>
-                  <a className={styles["menu__list-link"]} href="#team">
+                  <a
+                    className={styles["menu__list-link"]}
+                    href="#team"
+                    onClick={handleMenuLinkClick}
+                  >
                     OUR TEAM
                   </a>
                 </li>
                 <li className={styles["menu__list-item"]}>
-                  <a className={styles["menu__list-link"]} href="#contact">
+                  <a
+                    className={styles["menu__list-link"]}
+                    href="#contact"
+                    onClick={handleMenuLinkClick}
+                  >
                     CONTACT
                   </a>
                 </li>
